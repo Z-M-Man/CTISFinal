0a49a926b06133ace5fc5c5c41afeae2253feb55
Z-M-Man <112212530+Z-M-Man@users.noreply.github.com>
2024-03-29 01:27:22 -0400

    Most of website done
    More pages, more CSS, JavaScript menu

---
+++ script.js
@@ -0,0 +1,18 @@
+//On click, toggle between hiding and showing dropdown content
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+//Close menu if user clicks out of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
